6a03792e2de211eb133ac7ba15dce6c0405dbc46
Your Name <harshitupadhyay610@gmail.com>
2025-04-07 16:40:33 +0530

    up

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // base:"/insuranceProject/",
+  base: process.env.NODE_ENV === 'production' ? '/insuranceProject/' : '/',
   plugins: [react()],
-  base:"/insuranceProject/",
 })
 
   
