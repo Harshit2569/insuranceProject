99bd8f08b1b1e22635d538a0328c7635f6aeb788
Your Name <harshitupadhyay610@gmail.com>
2025-04-07 09:52:05 +0530

    project

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/insuranceProject",
+  base:"/",
   build: {
     outDir: 'dist',
     sourcemap: false, // Set to true if you want source maps in production
